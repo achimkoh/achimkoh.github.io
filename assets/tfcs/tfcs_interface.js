a8ba6d9e3f12da3d744c07c9bbe242a5b81e72a1
akkakkak <achim.koh@gmail.com>
2017-01-12 20:03:51 -0500

    updating scroll interface tfcs for jquery use in wp

--- assets/tfcs/tfcs_interface.js
+++ assets/tfcs/tfcs_interface.js
@@ -126,5 +126,19 @@ jQuery(document).ready(function($) {
             $('button.recorder').css("background-color", "#f0f0f0");
         }, 300);
     });
+
+    // smooth scroll to inner links instead of jumps
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': ($target.offset().top)
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 });
 
