6cc32109a6a81fb05cb4293459f91cdeffad10d6
akkakkak <achim.koh@gmail.com>
2017-01-29 17:06:27 -0500

    fix video-frame height, so that elements stay in position when video is minimized

--- assets/tfcs/tfcs_interface.js
+++ assets/tfcs/tfcs_interface.js
@@ -132,8 +132,8 @@ jQuery(document).ready(function($) {
     window.exercisePosition = document.getElementsByClassName("exercise")[0].offsetTop;
     window.phonemeGridPosition = document.getElementsByClassName("phoneme-grid")[0].offsetTop;
 
-    // fix video-frame height, so that elements stay in position when video is minimized
-    document.getElementById('video-frame').style.height = document.getElementById('video').offsetHeight;
+    // on mobile: fix video-frame height, so that elements stay in position when video is minimized
+    if(jQuery.browser.mobile){document.getElementById('video-frame').style.height = document.getElementById('video').offsetHeight;}
 
 });
 
